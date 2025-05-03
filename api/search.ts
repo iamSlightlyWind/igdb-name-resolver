bb449c74f491291e2342f2fc80ef037ddc3dd339
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2025-05-03 21:58:17 +0700

    Fix query format

--- api/search.ts
+++ api/search.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { nameSearch } from "../utils/common";
 
 export default async function search(req: Request, res: Response) {
-    let query = req.query as string;
+    const { query } = req.query;
+    console.log("Query:", query);
 
     if (!query) {
         return res.status(400).json({ message: "Query parameter is required" });
